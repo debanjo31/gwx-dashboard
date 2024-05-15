4113dccdba3d2f1039f83a63aa00640145f76f26
Irsael <debanjo31@gmail.com>
2024-05-15 16:14:27 +0100

    chore: Update favicon link and add GWX Dashboard title

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+  return (
+    <div className="w-full flex flex-col justify-center items-center h-screen">
+      <h1 className="text-4xl text-center text-[#F39C12]">GWX DASHBOARD</h1>
+      <p className="text-center">This is a Tailwind CSS example.</p>
+    </div>
+  );
+} // Add closing curly brace here
