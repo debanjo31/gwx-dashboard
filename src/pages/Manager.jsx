ac2ebb2dbe258d3272d739b051ea3dfc8345e247
Irsael <debanjo31@gmail.com>
2024-05-15 19:52:16 +0100

    refactor: Update navigation and content layout in Customer, Manager, and SuperAdmin pages

--- src/pages/Manager.jsx
+++ src/pages/Manager.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../componenets/Header";
 
 const Customer = () => {
+  const [menu, setMenu] = useState("Top destinations");
   return (
     <div className="w-full h-screen flex relative overflow-x-hidden">
       <div className="naviagtion bg-[#F39C12] text-white w-3/12 h-full p-2 fixed left-0 top-0">
         <h3 className="font-bold">Manager View</h3>
+        <div className="mt-12 text-white flex flex-col text-base justify-left gap-4">
+          <button onClick={() => setMenu("Dashboard")}>Dashboard</button>
+          <button onClick={() => setMenu("Station Stats")}>
+            Transaction Analysis
+          </button>
+          <button onClick={() => setMenu("Client Stats")}>Client Stats</button>
+        </div>
       </div>
       <div className="w-9/12 h-full ml-auto">
         <Header />
--- src/pages/SuperAdmin.jsx
+++ src/pages/SuperAdmin.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../componenets/Header";
 
-const Customer = () => {
+const SuperAdmin = () => {
+  const [menu, setMenu] = useState("Top destinations");
   return (
     <div className="w-full h-screen flex relative overflow-x-hidden">
       <div className="naviagtion bg-[#F39C12] text-white w-3/12 h-full p-2 fixed left-0 top-0">
         <h3 className="font-bold">Super Admin View</h3>
+        <div className="mt-12 text-white flex flex-col text-base justify-left gap-4">
+          <button onClick={() => setMenu("Dashboard")}>Dashboard</button>
+          <button onClick={() => setMenu("Station Stats")}>
+            Station Stats
+          </button>
+          <button onClick={() => setMenu("Shipment Stats")}>
+            Shipment Stats
+          </button>
+          <button onClick={() => setMenu("Client Stats")}>Client Stats</button>
+        </div>
       </div>
       <div className="w-9/12 h-full ml-auto">
         <Header />
@@ -14,4 +25,4 @@ const Customer = () => {
   );
 };
 
-export default Customer;
+export default SuperAdmin;
