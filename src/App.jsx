238ed671c7b7c0cdb876503cd32945c40a0b0650
Irsael <debanjo31@gmail.com>
2024-05-15 16:24:18 +0100

    feat: Add routes for Customer, Manager, and Super Admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-
+import Customer from "./pages/Customer";
+import SuperAdmin from "./pages/SuperAdmin";
+import Manager from "./pages/Manager";
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/manager" element={<Manager />} />
+        <Route path="/super-admin" element={<SuperAdmin />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center h-screen">
       <h1 className="text-4xl text-center text-[#F39C12]">GWX DASHBOARD</h1>
       <div className="my-8 flex gap-2">
-        <button>Customer</button>
-        <button>Manager</button>
-        <button>Super Admin</button>
+        <Link
+          to={"/customer"}
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Customer
+        </Link>
+        <Link
+          to={"/manager"}
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Manager
+        </Link>
+        <Link
+          to={"/super-admin"}
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Super Admin
+        </Link>
       </div>
     </div>
   );
